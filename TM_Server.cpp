e7549c45252ef70ee410a8af91808558e00d447e
Morgan <morgansstuart243@gmail.com>
2013-03-09 22:14:33 -0500

    Commenting, formatting, and a bit of refactoring
    No functional changes.

--- TM_Server.cpp
+++ TM_Server.cpp
@@ -19,11 +19,18 @@ using namespace std;
 
 TM_Server::TM_Server()
 {
+//{{{
+    //set hosting address and port
     TM_Server::address = "127.0.0.1";
     TM_Server::port = 1337;
+
+    //setup socket (if not already), set adress, and bind socket
     TM_Server::master_server.Init(true, TM_Server::address, TM_Server::port);
     cout<<"Server initialized on "<<TM_Server::address<<":"<<TM_Server::port<<endl;
+
+    //don't stop until done is true
     TM_Server::done = false;
+//}}}
 }
 
 TM_Server::~TM_Server()
@@ -42,14 +49,19 @@ TM_Server::~TM_Server()
 
 void TM_Server::SendMessage(TM_Message out_message, unsigned char out_buffer[])
 {
+//{{{
         //clear data buffer
         bzero(out_buffer, sizeof(out_buffer));
     
        //get string version of data
-       int size = sprintf((char*)out_buffer, "%c:%u:%u", out_message.code, out_message.address,out_message.value);
+       int size 
+           = sprintf((char*)out_buffer, "%c:%u:%u", out_message.code, out_message.address,out_message.value);
 
         cout<<"Message to send: "<<out_buffer<<endl;
+
+        //send out the formatted string
         TM_Server::master_server.Send(out_buffer, size);
+//}}}
 }
 
 TM_Message TM_Server::ReceiveMessage(string in_buffer, int client_id)
@@ -60,31 +72,51 @@ TM_Message TM_Server::ReceiveMessage(string in_buffer, int client_id)
 
     in_buffer.clear();
 
-    TM_Server::master_server.Receive(&in_buffer, 1024, client_id);
+    int bytes_recv = TM_Server::master_server.Receive(&in_buffer, 1024, client_id);
 
-    if(in_buffer != "SHUTDOWN")
+    //check for errors
+    if(bytes_recv < 0)
     {
-        pos1 = in_buffer.find_first_of(":");
-
-        temp_message.code = in_buffer[pos1 - 1];
+        cout<<"Error receiving message from client "<<client_id<<endl;
         
-        in_buffer[pos1] = '-';
-
-        pos2 = in_buffer.find_first_of(":");
-
-        temp_message.address = (unsigned int) atoi(in_buffer.substr(pos1+1, pos2-1).c_str());
-        
-        temp_message.address = (unsigned int) atoi(in_buffer.substr(pos2+1, in_buffer.length()).c_str());
-
-        cout<<hex<<"\tcode: "<<(unsigned int)temp_message.code<<endl;
-        cout<<hex<<"\taddr: "<<temp_message.address<<endl;
-        cout<<hex<<"\tvalue: "<<temp_message.value<<endl;
+        //indicate error in message struct
+        temp_message.code = 0;
+        temp_message.address = 0;
+        temp_message.value = 0;
     }
-    else
+    //check for client shutdown
+    else if(in_buffer == "SHUTDOWN")
     {
         temp_message.code = 0xFF;
         temp_message.address = 0;
         temp_message.value = 0;
+    }
+    //everything is good, parse the message
+    else
+    {
+        //find the index of the first colon
+        //(it will always be 1 since the first portion is one character)
+        //(implemented dynamic way in case the message encoding changes)
+        pos1 = in_buffer.find_first_of(":");
+
+        //get first character(currently always index zero)
+        temp_message.code = in_buffer[pos1 - 1];
+        
+        //replace with '-' so find gets the next one
+        in_buffer[pos1] = '-';
+
+        //get second position of colon
+        pos2 = in_buffer.find_first_of(":");
+
+        //get the address out
+        temp_message.address = (unsigned int) atoi(in_buffer.substr(pos1+1, pos2-1).c_str());
+        
+        //get the value out
+        temp_message.value = (unsigned int) atoi(in_buffer.substr(pos2+1, in_buffer.length()).c_str());
+
+        cout<<hex<<"\tcode: "<<(unsigned int)temp_message.code<<endl;
+        cout<<hex<<"\taddr: "<<temp_message.address<<endl;
+        cout<<hex<<"\tvalue: "<<temp_message.value<<endl;
     }
 
     return temp_message;
@@ -94,80 +126,89 @@ TM_Message TM_Server::ReceiveMessage(string in_buffer, int client_id)
 void TM_Server::Start_Server()
 {
 //{{{
-    int temp_socket, client_id;
     Connected_Client temp_client;
 
-   cout<<"Beginning listen..."<<endl; 
-       TM_Server::master_server.Listen(20);
+    cout<<"Beginning listen..."<<endl; 
+
+    //prepare for clients
+    TM_Server::master_server.Listen(20);
    
-   while(!done)
-   {
-       cout<<"Accepting new client..."<<endl;
-            client_id = TM_Server::master_server.Accept();
+    //loop continues until end
+    while(!done)
+    {
+        cout<<"Accepting new client..."<<endl;
 
-            temp_client.id = client_id;
+        //accept a client, get its id
+        temp_client.id = TM_Server::master_server.Accept();
 
-            connected_clients.push_back(temp_client);
+        //store the client in vector
+        connected_clients.push_back(temp_client);
 
+        cout<<"Launching client thread..."<<endl;
 
-       cout<<"Launching client thread..."<<endl;
-            connected_clients.back().client_thread = new std::thread(&TM_Server::LaunchClient, this, &connected_clients.back());
-   }
-
+        //launch the clients thread into LaunchClient
+        connected_clients.back().client_thread 
+                        = new std::thread(&TM_Server::LaunchClient, this, &connected_clients.back());
+    }
    //}}}
 }
-
 
 void TM_Server::LaunchClient(Connected_Client *client)
 {
 //{{{
     string user_in;
     client->client_done = false;
-    string in_buffer, out_buffer[1024];
-    int byte_count = 0;
 
     cout<<"Thread handling client with id "<<client->id<<endl;
 
+    //stop only if server stops or client shutdowns
     while(!this->done && !client->client_done)
     {
         //bzero(in_buffer.c_str(), 1024);
-        in_buffer.erase();
+        client->in_buffer.erase();
 
         cout<<"Receiving..."<<endl;
         //byte_count = TM_Server::master_server.Receive(&in_buffer, 1024, client->id);
 
         client->in_message = TM_Server::ReceiveMessage(client->in_buffer, client->id);
 
-        if(byte_count > 0)
-            cout<<"Received: "<<in_buffer<<endl;
-        else if (byte_count < 0)
-            printf("Error, return is: %d\n", byte_count);
-
-        if(client->in_message.code == 0xFF)
+        //check for error(==0)
+        if (!client->in_message.code)
+        {
+            cout<<"ERROR, ignoring..."<<endl;
+        }
+        //check for shutdown code from client
+        else if(client->in_message.code == 0xFF)
         {
             client->client_done = true;
         }
+        //otherwise, parse the message and respond
         else
         {
+            //check access cache here
             cout<<"Allow this operation? (y/n)"<<endl;
             cin>>user_in;
             
+            //allow transaction
             if(user_in == "y")
             {
+                //echo the message: output = input
                 client->out_message = client->in_message;
                 TM_Server::SendMessage(client->out_message, client->out_buffer); //echo back for ACK
             }
+            //deny transaction
             else if (user_in == "n")
             {
+                //set out message
                 client->out_message = client->in_message;
+
+                //send some abort code
                 client->out_message.code = 0x66;
                 TM_Server::SendMessage(client->out_message, client->out_buffer); //echo back for ACK
             }
         }
     }
 
-
     cout<<"Client Sent SHUTDOWN command, terminating thread..."<<endl;
 //}}}
 }
-
--- TM_Server.h
+++ TM_Server.h
@@ -3,47 +3,61 @@
 
 #include "../NetComm.git/NC_Server.h"
 
+//holds data that goes between client and server
 struct TM_Message
 {
-    unsigned char code;
-    unsigned int address;
-    unsigned int value;
+//{{{
+    unsigned char code;         //operation (read, write, commit, etc.)
+    unsigned int address;       //address of TM
+    unsigned int value;         //value of TM, not always used
+//}}}
 };
 
+//holds everything corresponding to a single client
 struct Connected_Client
 {
-    bool client_done;
-    std::thread *client_thread;
-    std::string name;
-    unsigned int id;
-    TM_Message out_message, in_message;
-    std::string in_buffer;
-    unsigned char out_buffer[1024];
-};
+//{{{
+    bool client_done;                   //has the client disconnected
+    std::string name;                   //friendly name of client
+    unsigned int id;                    //id used by network back end
 
+    TM_Message out_message, in_message; //temporary sending/receiving messages
+    std::string in_buffer;              //raw input string
+    unsigned char out_buffer[1024];     //output buffer
+
+    std::thread *client_thread;         //thread that's dealing with client
+//}}}
+};
 
 class TM_Server
 {
     private:
+        //is the server shutting down
         static bool done;
 
-
+        //listen address and port
         static std::string address;
         static unsigned int port;
+
+        //networking back end, will store sockets for all connected clients
         static NC_Server master_server;
+
+        //every clients client-specific data
         static std::vector<Connected_Client> connected_clients;
 
-
+        //parse and send out a TM_Message
         void SendMessage(TM_Message out_message, unsigned char out_buffer[]);
+
+        //receive data and put together a TM_Message
         TM_Message ReceiveMessage(std::string in_buffer, int client_id);
 
     public:
-
         TM_Server();
         ~TM_Server();
 
+        //start main loop, spawning threads for new clients
         void Start_Server();
 
+        //threads for each client fun here
         void LaunchClient(Connected_Client *client);
 };
-
